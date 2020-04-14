c05591aa6c79b72f199ae3821ff1e6a4d1cc3ede
JohnnyDevNull <philipp.john@live.de>
2020-04-14 23:17:04 +0200

    Replace Karma with Jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/**
+ * Configuration see:
+ * https://github.com/just-jeb/angular-builders/tree/master/packages/jest
+ * https://github.com/thymikee/jest-preset-angular
+ */
+module.exports = {
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  moduleFileExtensions: ['ts', 'js', 'html']
+};

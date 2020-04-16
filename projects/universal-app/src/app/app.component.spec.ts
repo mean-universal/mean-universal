944651f20e7507ca926ea6af7b818f0430b2aa81
JohnnyDevNull <philipp.john@live.de>
2020-04-16 12:00:04 +0200

    Optimize starter template
    * Added NestJS & MongoDB links
    * Replaces bootstrap with sanitize.css for a more un-opinionated start
    * fix project naming

--- projects/universal-app/src/app/app.component.spec.ts
+++ projects/universal-app/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'jparts-mean'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jparts-mean');
+    expect(app.title).toEqual('jpmean-universal');
   });
 
   it('should render title', () => {
